fc7a4597217495f2d0b900f1c780cb4b3f99572f
Prashant Bansal <prashantbansal500@gmail.com>
2023-12-18 01:01:53 +0530

    Database Connection

--- server/index.js
+++ server/index.js
@@ -1,4 +1,17 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGOURI)
+  .then(() => {
+    console.log("Connected to Database");
+  })
+  .catch((e) => {
+    console.log("Data not connected", e);
+  });
 
 const expressApp = express();
 
