90a2201fd36bb142075ef3fba673b5dcd8509a23
Prashant Bansal <prashantbansal500@gmail.com>
2023-12-25 18:12:05 +0530

    UI Fix

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,23 +4,26 @@ import PrivateRoute from "./components/PrivateRoute";
 
 const App = () => {
   return (
-    <BrowserRouter className="">
-      <Header />
-      <Routes>
-        <Route element={<PrivateRoute />}>
-          <Route path="/profile" element={<Profile />} />
-        </Route>
-        <Route element={<PrivateRoute />}>
-          <Route path="/" element={<Home />} />
-        </Route>
-        <Route element={<PrivateRoute />}>
-          <Route path="/about" element={<About />} />
-        </Route>
-        <Route path="/signin" element={<SignIn />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="*" element={<Navigate to="/" />} />
-      </Routes>
-    </BrowserRouter>
+    <div className="bg-gray-50 h-screen">
+      {" "}
+      <BrowserRouter className="">
+        <Header />
+        <Routes>
+          <Route element={<PrivateRoute />}>
+            <Route path="/profile" element={<Profile />} />
+          </Route>
+          <Route element={<PrivateRoute />}>
+            <Route path="/" element={<Home />} />
+          </Route>
+          <Route element={<PrivateRoute />}>
+            <Route path="/about" element={<About />} />
+          </Route>
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 };
 
